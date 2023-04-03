1f73e9d2d0ad255c3dc687b0603155ea9591de8d
灿烂 <1481086138@qq.com>
2023-04-03 21:36:33 +0800

    补充void类型细节

--- 01_typescript语法基础/13_TS中的类型-void.ts
+++ 01_typescript语法基础/13_TS中的类型-void.ts
@@ -28,6 +28,8 @@ console.log(parseLyrics('你是我的歌'))
 //   }, 1000)
 // }
 
+// 我们可以将undefined赋值给void类型，也就是函数可以返回undefined
+
 type delayfnType = (...args: any[]) => void
 function delayFn(fn: delayfnType) {
   setTimeout(() => {
@@ -38,5 +40,15 @@ function delayFn(fn: delayfnType) {
 delayFn((...args) => {
   console.log(...args)
 })
+
+function foo() :void {
+  // return 123 // 会报错
+}
+
+const list = [123,444,5555]
+list.forEach((item, index, arr) => {
+  return 123 // 不会报错
+})
+// 原因：当基于上下文的类型推导(Contextual Typing)推导出返回类型为 void 的时候，并不会强制函数一定不能返回内容
 
 export {}
