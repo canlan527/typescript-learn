85c180469044d5aefa4898fd82742fdcecdf9f6d
灿烂 <1481086138@qq.com>
2023-04-14 21:40:35 +0800

    类的readonly

---
+++ 03_typescript面向对象/04_类的readonly.ts
@@ -0,0 +1,15 @@
+// 
+class Person {
+  name: string
+  readonly age: number
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const p = new Person('jxz', 18)
+
+console.log(p.name)
+p.name = 'wsx'
+// p.age = 20  // 只读属性不能修改
