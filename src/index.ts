dd45dafbfd2c77d0bab1635a604a9569379545fb
灿烂 <1481086138@qq.com>
2023-04-27 14:06:46 +0800

    定义AxiosRequestConfig类型

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-// function axios(config) {
+import { AxiosRequestConfig } from './types/index';
 
-// }
+// 作为库的入口文件
+function axios(config: AxiosRequestConfig) {
+  // ..
 
-// export default axios
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// 类型定义的公共文件
+export type Method = 'get' | 'GET'
+ | 'post' | 'POST'
+ | 'delete' | 'DELETE'
+ | 'put' | 'PUT'
+ | 'patch' | 'PATCH'
+ | 'head' | 'HEAD'
+ | 'options' | 'OPTIONS';
+
+export interface AxiosRequestConfig {
+  url: string,
+  method?: Method,
+  data?: any,
+  params?: any
+}
