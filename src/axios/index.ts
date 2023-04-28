b88b1c340ce8a4394fd3faa4cf0af3c2376ca3e9
灿烂 <1481086138@qq.com>
2023-04-28 16:23:14 +0800

    处理请求 url 参数 - 实现 url 参数处理逻辑

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,10 +1,21 @@
-import { AxiosRequestConfig } from './types';
-import xhr from './xhr';
+import { AxiosRequestConfig } from "./types";
+import xhr from "./xhr";
+import { buildURL } from "./helpers/url";
 
 // 作为库的入口文件
-function axios(config: AxiosRequestConfig):void {
-  // ..
-  xhr(config)
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config);
+  xhr(config);
 }
 
-export default axios
+// 处理config
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config);
+}
+// 处理config的url和params
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config;
+  return buildURL(url, params);
+}
+
+export default axios;
