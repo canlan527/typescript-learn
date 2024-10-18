f33b97fbdd76a97dd6405f757b40a99d79dc3369
灿烂 <1481086138@qq.com>
2024-10-18 15:49:33 +0800

    chapter2

---
+++ chapter2/src/index.ts
@@ -0,0 +1,14 @@
+console.log('hello typescript')
+
+// 练习题
+let a = 1 + 2
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple * 4
+
+// 尝试一些无效操作并修正
+// let e =  a + c
+let e = a + c.banana
